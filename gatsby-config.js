c3b8e1d1a910655e825648467a5cf6ed29bd507c
Olaf Sulich <olafsulich@gmail.com>
2020-10-26 16:58:03 +0100

    Replace static paths to images with dynamic paths from graphql queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,16 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+const path = require(`path`);
 
 module.exports = {
-  /* Your site config here */
   plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `assets`, `images`),
+      },
+    },
     {
       resolve: `gatsby-plugin-sass`,
       options: {
